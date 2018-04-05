06759698070d17023acfc85040c62485c84f5268
danvanhorn <danvanhorn17@gmail.com>
2018-04-04 19:47:55 -0700

    fix tmux errors

--- home/.alias.zsh
+++ home/.alias.zsh
@@ -6,10 +6,15 @@ alias tnew='tmux new -s'
 alias tls='tmux ls'
 alias tkill='tmux kill-session -t'
 
+# better cmds
+alias ls='ls -pla'
+
+
 # convenience aliases for editing configs
 alias ev='vim ~/.vimrc'
 alias et='vim ~/.tmux.conf'
 alias ez='vim ~/.zshrc'
+alias ea='vim ~/.alias.zsh'
 
 # connect to the school server
 alias engr='ssh vanhornd@access.engr.oregonstate.edu'
@@ -18,3 +23,6 @@ alias engr='ssh vanhornd@access.engr.oregonstate.edu'
 # only usable when the OSX plugin is active
 alias splay='spotify play'
 alias sp='spotify'
+
+alias cs444='cd ~/cs444'
+
